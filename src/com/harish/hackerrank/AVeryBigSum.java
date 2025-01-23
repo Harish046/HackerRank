06a9755db19b0949bead6c13cad5852c971c01a1
Harish Rao S <harishrao.10824649@ltimindtree.com>
2025-01-23 16:36:07 +0530

    A Very Big Sum closes #5

---
+++ src/com/harish/hackerrank/AVeryBigSum.java
@@ -0,0 +1,41 @@
+package com.harish.hackerrank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
+
+public class AVeryBigSum {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        long result = aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+	
+	public static long aVeryBigSum(List<Long> ar) {
+	        Long result=0L;
+	        for (Long long1 : ar) {
+	            result=result+long1;
+	        }
+	        return result;
+	    }
+
+}
