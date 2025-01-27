e307f8dedbad9590f95b4879279dbc5a9c4bd11b
Harish Rao S <harishrao.10824649@ltimindtree.com>
2025-01-27 11:24:03 +0530

    Arrays - DS closes #9

---
+++ src/com/harish/hackerrank/ArrayDS.java
@@ -0,0 +1,52 @@
+package com.harish.hackerrank;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ArrayDS {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> res = reverseArray(arr);
+
+        bufferedWriter.write(
+            res.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+	
+	public static List<Integer> reverseArray(List<Integer> a) {
+        //using built in function
+        Collections.reverse(a); 
+        return a;
+       
+       //Using loop
+       /*List<Integer> result=new ArrayList<>();
+       for( int i=a.size()-1;i>=0;i--)
+           result.add(a.get(i));
+        return result;*/
+}
+
+}
